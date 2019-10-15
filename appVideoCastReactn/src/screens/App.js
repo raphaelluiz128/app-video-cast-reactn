f88db5817f8af4e2237b8745af188a05a243f8a7
raphaelluizschettino <raphaelluiz128@hotmail.com>
2019-10-15 00:53:04 -0300

    Create config for android 9 compatible webview, video and external requests
    The android 9 have more config for security external resources

--- appVideoCastReactn/src/screens/App.js
+++ appVideoCastReactn/src/screens/App.js
@@ -6,18 +6,33 @@
  * @flow
  */
 import React, { Component } from 'react';
-import { WebView } from 'react-native-webview';
-import { SafeAreaView,StyleSheet,ScrollView,View,Text,StatusBar,} from 'react-native';
+import Video from 'react-native-video';
+import { SafeAreaView,StyleSheet,ScrollView,View,Text,StatusBar,Dimensions} from 'react-native';
 import {Header,LearnMoreLinks,Colors,DebugInstructions,ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 export default class App extends Component {
+
+
   render(){
     return(
-    <View style={styles.container}>
-         <WebView source={{ uri: 'https://www.youtube.com/embed/6c0LA4Nb1vY' }} />
-    </View>
+      <View style={styles.container}>
+           <Video source={{uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"}}   // Can be a URL or a local file.
+       ref={(ref) => {
+         this.player = ref
+       }}                                      // Store reference
+       onBuffer={this.onBuffer}                // Callback when remote video is buffering
+       onError={this.videoError}               // Callback when video cannot be loaded
+       style = {styles.backgroundVideo}/>
+       </View> 
     );}}
     const styles = StyleSheet.create({
-      container:{ flex: 1, justifyContent: "center", backgroundColor:"red", height: 300},
+      container:{flex:1},
+      backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+      },
     });
