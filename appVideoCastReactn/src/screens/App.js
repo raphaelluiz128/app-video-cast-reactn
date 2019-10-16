2b7e78aa359c074767ccdd4dca976630cececc28
raphaelluizschettino <raphaelluiz128@hotmail.com>
2019-10-16 01:37:49 -0300

    Add config for android >= 8.1 external acess
    This config is diferent for android < 8.1

--- appVideoCastReactn/src/screens/App.js
+++ appVideoCastReactn/src/screens/App.js
@@ -17,22 +17,23 @@ export default class App extends Component {
   render(){
     return(
       <View style={styles.container}>
-           <Video source={{uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"}}   // Can be a URL or a local file.
+           <Video ignoreSslError={true}  source={{uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"}}   // Can be a URL or a local file.
        ref={(ref) => {
          this.player = ref
        }}                                      // Store reference
        onBuffer={this.onBuffer}                // Callback when remote video is buffering
        onError={this.videoError}               // Callback when video cannot be loaded
-       style = {styles.backgroundVideo}/>
+       style = {styles.backgroundVideo}
+       />
        </View> 
     );}}
     const styles = StyleSheet.create({
-      container:{flex:1},
+      container:{flex:1, backgroundColor:'#2b78e4'},
       backgroundVideo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
+        display:'flex',
+        flex:1,
+       flexDirection:'column',
+       alignItems:'center',
+       justifyContent:'center',
       },
     });
